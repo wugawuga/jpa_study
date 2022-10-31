c5c4a0d591707f51c81942134a247abcb13048b4
wugawuga <dnr3655@naver.com>
2022-10-31 22:54:52 +0900

    feat: @ManyToOne, @JoinColumn 단방향

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,16 +19,9 @@ public class Member {
 	@Column(name = "USERNAME")
 	private String name;
 
-	@Column(name = "TEAM_ID")
-	private Long teamId;
-
-	public Long getTeamId() {
-		return teamId;
-	}
-
-	public void setTeamId(Long teamId) {
-		this.teamId = teamId;
-	}
+	@ManyToOne
+	@JoinColumn(name = "TEAM_ID")
+	private Team team;
 
 	private String city;
 	private String street;
