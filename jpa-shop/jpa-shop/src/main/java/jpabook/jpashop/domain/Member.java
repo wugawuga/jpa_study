0e24084365ed8528e49ae744d9355f159fa4d10d
wugawuga <dnr3655@naver.com>
2022-11-01 23:33:16 +0900

    feat: 양방향 연관관계 - mappedby 이해하기, 객체와 테이블 매핑

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
 public class Member {
@@ -71,7 +70,8 @@ public class Member {
 		return team;
 	}
 
-	public void setTeam(Team team) {
+	public void changeTeam(Team team) {
 		this.team = team;
+		team.getMembers().add(this);
 	}
 }
--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Team.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Team.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Team {
@@ -13,7 +17,14 @@ public class Team {
 	@Column(name = "TEAM_ID")
 	private Long id;
 
+	@OneToMany(mappedBy = "team")
+	private List<Member> members = new ArrayList<>();
+
 	private String name;
+
+	public List<Member> getMembers() {
+		return members;
+	}
 
 	public Long getId() {
 		return id;
