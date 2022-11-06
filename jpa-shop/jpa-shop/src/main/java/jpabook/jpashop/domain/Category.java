612ab79cbc28ff22f3675d7ecf3063b9c6ec7a15
wugawuga <dnr3655@naver.com>
2022-11-06 23:15:03 +0900

    refactor: 다대다 중간 테이블 매핑

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -25,4 +27,11 @@ public class Category {
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
+    )
+    private List<Item> items = new ArrayList<>();
 }
