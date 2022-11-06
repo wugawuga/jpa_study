839aa48dd30bc0a90b5772678fb0d804f6b57f20
wugawuga <dnr3655@naver.com>
2022-11-06 23:15:52 +0900

    refactor: 다대다 양방향 매핑

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,49 +1,56 @@
 package jpabook.jpashop.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Item {
 
-	@Id @GeneratedValue
-	@Column(name = "ITEM_ID")
-	private Long id;
-	private String name;
-	private int price;
-	private int stockQuantity;
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
 
-	public Long getId() {
-		return id;
-	}
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getPrice() {
-		return price;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setPrice(int price) {
-		this.price = price;
-	}
+    public int getPrice() {
+        return price;
+    }
 
-	public int getStockQuantity() {
-		return stockQuantity;
-	}
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
-	public void setStockQuantity(int stockQuantity) {
-		this.stockQuantity = stockQuantity;
-	}
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
