0be1b778e21d6350f0e5e6f3d627949a4b63f27a
wugawuga <dnr3655@naver.com>
2022-11-07 23:44:48 +0900

    feat: 상속관계 매핑을 위한 엔티티 추가

---
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Book.java
@@ -0,0 +1,7 @@
+package jpabook.jpashop.domain;
+
+public class Book extends Item {
+
+    private String author;
+    private String isbn;
+}
---
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Movie.java
@@ -0,0 +1,7 @@
+package jpabook.jpashop.domain;
+
+public class Movie extends Item {
+
+    private String director;
+    private String actor;
+}
