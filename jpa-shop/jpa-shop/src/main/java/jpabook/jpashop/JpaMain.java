6091d9db497b861e77619e60d88d77ef951d968d
wugawuga <dnr3655@naver.com>
2022-11-10 23:00:12 +0900

    test: 단일 테이블 전략 테스트

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Member;
 
 public class JpaMain {
@@ -16,12 +17,11 @@ public class JpaMain {
 
 		try {
 
-			Member member = new Member();
-			member.setCreateBy("kim");
-			member.setName("user1");
-			member.setCreateDate(LocalDateTime.now());
+			Book book = new Book();
+			book.setName("wuga");
+			book.setAuthor("정욱");
 
-			em.persist(member);
+			em.persist(book);
 
 			tx.commit();
 		} catch (Exception e) {
