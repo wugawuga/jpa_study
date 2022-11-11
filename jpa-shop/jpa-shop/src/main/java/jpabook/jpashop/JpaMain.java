0afd2f731a8f46e44dd52e48e251f5b093fb5ac1
wugawuga <dnr3655@naver.com>
2022-11-11 23:32:55 +0900

    refactor : 프록시 객체 초기화

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,35 +1,34 @@
 package jpabook.jpashop;
 
-import java.time.LocalDateTime;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Member;
 
 public class JpaMain {
-	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("shop");
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("shop");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
-		try {
+        try {
+            Member member = new Member();
+            member.setName("wuga");
 
-			Book book = new Book();
-			book.setName("wuga");
-			book.setAuthor("정욱");
+            em.persist(member);
 
-			em.persist(book);
+            Member findMember = em.getReference(Member.class, member.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
 
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-		} finally {
-			em.close();
-		}
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
-		emf.close();
-	}
+        emf.close();
+    }
 }
