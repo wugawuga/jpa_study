2436e6e9da0bfec2e4659be3345e344d180c28f5
wugawuga <dnr3655@naver.com>
2022-11-16 23:44:29 +0900

    refactor: 지연 로딩

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,76 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Member extends BaseEntity {
 
-	@Id
-	@GeneratedValue
-	@Column(name = "MEMBER_ID")
-	private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
 
-	@Column(name = "USERNAME")
-	private String name;
+    @Column(name = "USERNAME")
+    private String name;
 
-	@OneToMany(mappedBy = "member")
-	private List<Order> orders = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
-	@ManyToOne
-	@JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
-	private Team team;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
-	@OneToOne
-	@JoinColumn(name = "LOCKER_ID")
-	private Locker locker;
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
-	private String city;
-	private String street;
-	private String zipcode;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-
-	public void setStreet(String street) {
-		this.street = street;
-	}
-
-	public String getZipcode() {
-		return zipcode;
-	}
-
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
+    private String city;
+    private String street;
+    private String zipcode;
 }
--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Team.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Team.java
@@ -8,8 +8,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Team extends BaseEntity {
 
     @Id
@@ -22,24 +26,4 @@ public class Team extends BaseEntity {
     private List<Member> members = new ArrayList<>();
 
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Member> getMembers() {
-        return members;
-    }
 }
