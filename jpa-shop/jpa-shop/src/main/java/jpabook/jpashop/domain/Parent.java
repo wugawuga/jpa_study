d76a0fe34f52c1ecb3139af8ced8572879c04bed
wugawuga <dnr3655@naver.com>
2022-11-18 23:33:25 +0900

    feat: 고아 객체 활성화

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Parent.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Parent.java
@@ -17,7 +17,7 @@ public class Parent {
 
     private String name;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Child> childList = new ArrayList<>();
 
     public void addChild(Child child) {
@@ -39,5 +39,9 @@ public class Parent {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Child> getChildList() {
+        return childList;
     }
 }
