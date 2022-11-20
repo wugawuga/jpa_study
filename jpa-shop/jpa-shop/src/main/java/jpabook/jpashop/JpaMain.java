3ca908e76f09f0fcde624f11e54995184ce86fa1
wugawuga <dnr3655@naver.com>
2022-11-20 23:05:46 +0900

    test: 임베디드 타입 확인

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,14 +1,12 @@
 package jpabook.jpashop;
 
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import jpabook.jpashop.domain.Child;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Parent;
-import jpabook.jpashop.domain.Team;
+import jpabook.jpashop.domain.Period;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,20 +17,11 @@ public class JpaMain {
 
         try {
 
-            Child child1 = new Child();
-            Child child2 = new Child();
-
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
-
-            em.persist(parent);
-
-            em.flush();
-            em.clear();
-
-            Parent findParent = em.find(Parent.class, parent.getId());
-            em.remove(findParent);
+            Member member = new Member();
+            member.setName("wuga");
+            member.setAddress(new Address("오산", "street", "1111"));
+            member.setPeriod(new Period());
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
