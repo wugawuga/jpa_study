930dbbd8fd81dea8b80d5ae11ee86cbf215cc0d3
wugawuga <dnr3655@naver.com>
2022-11-22 23:47:14 +0900

    feat: 값 타입 컬렉션 적용

--- jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpa-shop/jpa-shop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -44,4 +48,17 @@ public class Member {
 
     @Embedded
     private Address address;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+    @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+    @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressHistory = new ArrayList<>();
 }
