fed3ae868ba08f4600fded50ad2bdef9833ccd78
wugawuga <dnr3655@naver.com>
2022-11-27 22:46:05 +0900

    feat: jpql entity 추가

--- jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -1,29 +1,29 @@
 package hellojpa;
 
+import hellojpa.jpql.Member;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
-	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		tx.begin(); // 트랜잭션 시작
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // 트랜잭션 시작
 
-		try {
-			Member memberA = new Member();
-			memberA.setUsername("C");
+        try {
+            Member memberA = new Member();
+            memberA.setUsername("member1");
+            em.persist(memberA);
 
-			em.persist(memberA);
-
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-		} finally {
-			em.close();
-		}
-		emf.close();
-	}
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
 }
