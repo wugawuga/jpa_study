7d2bb6419cf8ff7a2357399f993e54a4e4aeaa93
wugawuga <dnr3655@naver.com>
2022-11-27 22:49:39 +0900

    test: TypeQuery, Query 테스트 - TypeQuery 는 반환 타입이 명확할 때 사용 - Query 는 반환 타입이 명확하지 않을 때 사용

--- jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,7 +18,12 @@ public class JpaMain {
         try {
             Member memberA = new Member();
             memberA.setUsername("member1");
+            memberA.setAge(10);
             em.persist(memberA);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query1 = em.createQuery("select m.username from Member m", String.class);
+            Query query2 = em.createQuery("select m.username, m.age from Member m");
 
             tx.commit();
         } catch (Exception e) {
