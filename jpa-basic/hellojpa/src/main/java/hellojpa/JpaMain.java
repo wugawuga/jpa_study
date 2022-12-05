9b0aa8e90ac438916129cbacfd796af3102e5c5e
wugawuga <dnr3655@naver.com>
2022-12-05 23:20:00 +0900

    test: jpql NULLIF 이름이 관리자면 null 반환

--- jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -33,7 +33,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
+            String query = "select nullif(m.username, '관리자') from Member m";
             List<String> result = em.createQuery(query, String.class).getResultList();
 
             for (String s : result) {
