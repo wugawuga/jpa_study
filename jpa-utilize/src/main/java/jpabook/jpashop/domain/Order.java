6a497d3d41c0a2fc371397c82e3472b05564b61e
wugawuga <dnr3655@naver.com>
2022-12-09 00:31:05 +0900

    refactor: 지연 로딩 설정

--- jpa-utilize/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-utilize/src/main/java/jpabook/jpashop/domain/Order.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -28,7 +29,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
