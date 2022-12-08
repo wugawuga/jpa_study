7823653e75c26e116daf14b4a9c09d75813d9b34
wugawuga <dnr3655@naver.com>
2022-12-08 22:21:02 +0900

    feat: 엔티티 1차 설계

--- jpa-utilize/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpa-utilize/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- jpa-utilize/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpa-utilize/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -2,11 +2,11 @@ package jpabook.jpashop;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
