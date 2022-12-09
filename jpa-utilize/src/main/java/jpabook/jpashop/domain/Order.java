ca52563d67ad40653c5d0278e97c7fac4c8c497b
wugawuga <dnr3655@naver.com>
2022-12-09 12:53:06 +0900

    feat: cascade 설정

--- jpa-utilize/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-utilize/src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -33,7 +34,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     @OneToOne(fetch = FetchType.LAZY)
