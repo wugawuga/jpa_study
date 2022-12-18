f81af9bd77012c3b33b489b788112402f4ca9405
wugawuga <dnr3655@naver.com>
2022-12-18 14:51:24 +0900

    feat: 간단 주문 조회, 엔티티 직접 노출

--- jpa-utilize/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpa-utilize/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,22 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module.Feature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+    }
 
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module module = new Hibernate5Module();
+//        module.configure(Feature.FORCE_LAZY_LOADING, true);
+        return module;
+    }
 }
