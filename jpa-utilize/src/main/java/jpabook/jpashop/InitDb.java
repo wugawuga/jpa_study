cfdcab886191bbb78697b94dd9c0aa63eeda2c84
wugawuga <dnr3655@naver.com>
2022-12-18 14:16:15 +0900

    feat: 조회용 샘플 데이터 입력

---
+++ jpa-utilize/src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,94 @@
+package jpabook.jpashop;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit1();
+        initService.dbInit2();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+
+        public void dbInit1() {
+            Member member = createMember("userA", "서울", "1", "1111");
+            em.persist(member);
+
+            Book book1 = createBook("JPA1 BOOK", 10000, 100);
+            em.persist(book1);
+
+            Book book2 = createBook("JPA2 BOOK", 20000, 100);
+            em.persist(book2);
+
+            Delivery delivery = createDelivery(member);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        public void dbInit2() {
+            Member member = createMember("userB", "오산", "2", "2222");
+            em.persist(member);
+
+            Book book1 = createBook("SPRING1 BOOK", 30000, 100);
+            em.persist(book1);
+
+            Book book2 = createBook("SPRING2 BOOK", 25000, 100);
+            em.persist(book2);
+
+            Delivery delivery = createDelivery(member);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 30000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 25000, 2);
+
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        private static Delivery createDelivery(Member member) {
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            return delivery;
+        }
+
+        private static Book createBook(String bookName, int price, int quantity) {
+            Book book1 = new Book();
+            book1.setName(bookName);
+            book1.setPrice(price);
+            book1.setStockQuantity(quantity);
+            return book1;
+        }
+
+        private Member createMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
+        }
+    }
+
+}
--- jpa-utilize/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-utilize/src/main/java/jpabook/jpashop/domain/Order.java
@@ -37,7 +37,7 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
