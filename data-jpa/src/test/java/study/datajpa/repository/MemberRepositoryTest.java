ab601eae7c15f39061f0d05b71637a589d95371b
wugawuga <dnr3655@naver.com>
2022-12-25 14:47:28 +0900

    feat: 사용자 정의 리포지토리 - 무조건 custom 만들어서 하는 건 옳지 않다 - 비즈니스 로직, 단순 조회용 로직 클래스 분리 추천

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -216,6 +216,10 @@ class MemberRepositoryTest {
 
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
-     
+    }
+
+    @Test
+    public void callCustom() {
+        List<Member> memberCustoms = memberRepository.findMemberCustom();
     }
 }
