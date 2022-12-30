8a5173ad0cfd5b2985468bdf14e5161d7e3b9c4e
wugawuga <dnr3655@naver.com>
2022-12-30 23:04:24 +0900

    feat: 조인-on절

--- jpa-querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ jpa-querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -259,4 +259,41 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    //예) 회원과 팀을 조인하면서, 팀 이름이 teamA 인 팀만 조인, 회원은 모두 조회
+    //JPQL: select m, t from Member m left join m.team t on t.name = 'teamA'
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(QMember.member, QTeam.team)
+                .from(QMember.member)
+//                .leftJoin(QMember.member.team, QTeam.team).on(QTeam.team.name.eq("teamA"))
+                .join(QMember.member.team, QTeam.team)
+//                .on(QTeam.team.name.eq("teamA"))
+                .where(QTeam.team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    // 연관관계가 없는 엔티티 외부 조인
+    // 회원의 이름이 팀 이름과 같은 대상 외부 조인
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(QMember.member, QTeam.team)
+                .from(QMember.member)
+                .leftJoin(QTeam.team).on(QMember.member.username.eq(QTeam.team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
