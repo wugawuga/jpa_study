ab2c2f0075380ef7bf985495876fc4989a7eb838
wugawuga <dnr3655@naver.com>
2023-01-02 12:21:31 +0900

    feat: @QueryProjection

--- jpa-querydsl/src/main/java/study/querydsl/dto/MemberDto.java
+++ jpa-querydsl/src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 
 @Data
@@ -13,6 +14,7 @@ public class MemberDto {
     public MemberDto() {
     }
 
+    @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
